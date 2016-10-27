eb3b97f811e0e6f79a578c1a919250f73d3a7aff
Wilfred Ruck <wilfredoruck@gmail.com>
2016-10-27 07:27:09 -0400

    changed price because .1 wasn't valid

--- customShop/app/controllers/charges_controller.rb
+++ customShop/app/controllers/charges_controller.rb
@@ -6,7 +6,7 @@ class ChargesController < ApplicationController
 	def create
 
 		#Amount in cents
-		@amount = 0.100
+		@amount = 10
 
 		customer = Stripe::Customer.create(
 			:email => params[:stripeEmail],
