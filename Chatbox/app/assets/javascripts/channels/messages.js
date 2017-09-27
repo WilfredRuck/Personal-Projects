1e086d82707a613fdb09bad3c3ee4ce54f56afc1
Wilfred Ruck <wilfredoruck@gmail.com>
2017-09-27 06:23:04 -0400

    chatroom scrolls very close to bottom, but not all the way when new message is loaded

--- Chatbox/app/assets/javascripts/channels/messages.js
+++ Chatbox/app/assets/javascripts/channels/messages.js
@@ -5,13 +5,23 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
 
   renderMessage: function(data) {
   	document.getElementById("message-box").value = "";
-    return "<p> <b>" + data.user + "(" + data.time + "): </b>" + data.message + "</p>";
+	updateScroll();
+    return "<div> <b> (" + data.time + ")" + data.user + ": </b>" + data.message + "</div>";
   }
 
 
 });
 
+$("#chatroom-scroll").animate({ scrollTop: $('#chatroom-scroll').prop("scrollHeight")}, 1000);
+
 $(document).on('turbolinks:load', function() {
     $('input[type=submit]', this).attr('disabled', false)
+    var element = document.getElementById("chatroom-scroll");
+    element.scrollTop = element.scrollHeight;
 });
+
+function updateScroll(){
+    var element = document.getElementById("chatroom-scroll");
+    element.scrollTop = element.scrollHeight;
+}
 
