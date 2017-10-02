3d6e4445ca751e8ff97cb29d2dd9dc2e8b059a9a
Wilfred Ruck <wilfredoruck@gmail.com>
2017-10-01 21:18:21 -0400

    4 letters 2 numbers instead of 4 numbers 2 letters

--- CodeGenerator/app/controllers/code_controller.rb
+++ CodeGenerator/app/controllers/code_controller.rb
@@ -7,19 +7,16 @@ class CodeController < ApplicationController
   def create
   	code = ''
   	while(code == '')
-	  	firstNums = rand(0...99).to_s
-	  	if firstNums.length == 1
-	  		firstNums.insert(0,'0')
+	  	firstLetters = ('A'..'Z').to_a.shuffle[0,2].join
+	  	
+	  	numbers = rand(0...99).to_s
+	  	if numbers.length == 1
+	  		numbers.insert(0,'0')
 	  	end
 
-	  	letters = ('A'..'Z').to_a.shuffle[0,2].join
+	  	secondLetters = ('A'..'Z').to_a.shuffle[0,2].join
 
-	  	secondNums = rand(0...99).to_s
-	  	if secondNums.length == 1
-	  		secondNums.insert(0,'0')
-	  	end
-
-	  	code = (firstNums + letters + secondNums)
+	  	code = (firstLetters + numbers + secondLetters)
 	  	if (Code.where(code: code).any? == false)
 		  	Code.create(code: code)
 	  		redirect_to root_path
