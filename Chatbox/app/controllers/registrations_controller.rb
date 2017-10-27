26d0017150e31b18df0095c9f035954353db6f7a
Wilfred Ruck <wilfredoruck@gmail.com>
2017-10-27 16:59:05 -0400

    enabled user avatars

--- Chatbox/app/controllers/registrations_controller.rb
+++ Chatbox/app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :avatar)
   end
 end
