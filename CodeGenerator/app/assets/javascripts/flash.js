a16ac85323767fa33f9a76699961a7ff83d6f8d5
Wilfred Ruck <wilfredoruck@gmail.com>
2017-11-09 17:16:29 -0500

    styling updates

--- CodeGenerator/app/assets/javascripts/flash.js
+++ CodeGenerator/app/assets/javascripts/flash.js
@@ -3,3 +3,11 @@
       $('#success').fadeOut('slow');
     }, 5000); // <-- time in milliseconds
 });
+
+ function copyText(code) {
+  var copyText = document.getElementById("#random-code");
+  console.log(copyText);
+  code.select();
+  document.execCommand("Copy");
+  alert("Copied!");
+}
