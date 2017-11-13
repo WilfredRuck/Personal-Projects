6a11216286eeaf256ed96c796100a249155d133d
Wilfred Ruck <wilfredoruck@gmail.com>
2017-11-13 03:17:56 -0500

    added message when code is copied

--- CodeGenerator/app/assets/javascripts/flash.js
+++ CodeGenerator/app/assets/javascripts/flash.js
@@ -1,9 +1,13 @@
  $(document).ready(function(){
     setTimeout(function() {
       $('#success').fadeOut('slow');
-    }, 5000); // <-- time in milliseconds
+    }, 3000); // <-- time in milliseconds
 });
 
  function copyText(code) {
-  clipboard.writeText(code);
+	clipboard.writeText(code);
+  
+	$('#copied').append("Copied!");
+
+	$('#copied').fadeOut(3000);
 }
