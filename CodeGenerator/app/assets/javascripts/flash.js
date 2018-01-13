16ef78e568bad04438c51bda0da8db2bfd075fff
Wilfred Ruck <wilfredoruck@gmail.com>
2018-01-12 21:03:40 -0500

    added comments to javascript copy function

--- CodeGenerator/app/assets/javascripts/flash.js
+++ CodeGenerator/app/assets/javascripts/flash.js
@@ -4,9 +4,10 @@
     }, 3000); // <-- time in milliseconds
 });
 
+ // Initiates the copying of the code created
  function copyText(code) {
 	clipboard.writeText(code);
-  
+  	// Outputs the text "Copied" to user for confirmation, fades after 3 seconds
 	$('#copied').append("Copied!");
 
 	$('#copied').fadeOut(3000);
