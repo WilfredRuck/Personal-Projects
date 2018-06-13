08b6f2054ccb4c5f9cb3748e4477a783ffc19cc1
Wilfred Ruck <wilfredoruck@gmail.com>
2018-06-13 02:02:50 -0400

    rearrange order of controller functions

--- CodeGenerator/app/controllers/code_controller.rb
+++ CodeGenerator/app/controllers/code_controller.rb
@@ -5,31 +5,31 @@ class CodeController < ApplicationController
   	@codes = Code.all.reverse
   end
 
-  def create
-  	code = ''
-  	while(code == '')
-	  	firstLetters = ('A'..'Z').to_a.shuffle[0,2].join
-	  	
-	  	numbers = rand(0...99).to_s
-	  	if numbers.length == 1
-	  		numbers.insert(0,'0')
-	  	end
-
-	  	secondLetters = ('A'..'Z').to_a.shuffle[0,2].join
-
-	  	code = (firstLetters + numbers + secondLetters)
-	  	if (Code.where(code: code).any? == false)
-		  	Code.create(code: code)
-		  	flash[:code] = code
-	  		redirect_to root_path
-	  	else
-	  		code = ''
-	  	end
-	  end
-  end
-
   def new
   	@code = Code.new
+  end
+
+  def create
+    code = ''
+    while(code == '')
+      firstLetters = ('A'..'Z').to_a.shuffle[0,2].join
+      
+      numbers = rand(0...99).to_s
+      if numbers.length == 1
+        numbers.insert(0,'0')
+      end
+
+      secondLetters = ('A'..'Z').to_a.shuffle[0,2].join
+
+      code = (firstLetters + numbers + secondLetters)
+      if (Code.where(code: code).any? == false)
+        Code.create(code: code)
+        flash[:code] = code
+        redirect_to root_path
+      else
+        code = ''
+      end
+    end
   end
 
   def adminCreate
