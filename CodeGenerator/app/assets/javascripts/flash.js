bad89dd3ce0abe8589a6dc7b6a344dc1a23cec15
Wilfred Ruck <wilfredoruck@gmail.com>
2019-01-12 10:43:56 -0500

    create slideUp functionality
    make code gen section slide up after 2 seconds of clicking the copy button

--- CodeGenerator/app/assets/javascripts/flash.js
+++ CodeGenerator/app/assets/javascripts/flash.js
@@ -1,14 +1,12 @@
-$(document).ready(function(){
-    setTimeout(function() {
-      $('#success').fadeOut('slow');
-    }, 3000); // <-- time in milliseconds
-});
-
  // Initiates the copying of the code created
- function copyText(code) {
+function copyText(code) {
 	clipboard.writeText(code);
-  	// Outputs the text "Copied" to user for confirmation, fades after 3 seconds
 	$('#copied').append("Copied!");
-
-	$('#copied').fadeOut(3000);
+	// Outputs the text "Copied" to user for confirmation
+	setTimeout(() => {
+		$('#success').fadeOut('slow');
+		$('.code-gen-section').slideToggle("slow");
+		$('#copied').slideToggle("slow");
+	}, 2000);
+	
 }
